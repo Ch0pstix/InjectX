235ff26c2f3446242dad82b51a11c6e260586b64
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-22 18:01:20 -0500

    Create SkipRegistrationAttribute.cs

---
+++ src/InjectX/SkipRegistrationAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace InjectX;
+
+/// <summary>
+/// Specifies that the object associated with this attribute should be skipped during service registration.<br/>
+/// This class cannot be inherited.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class SkipRegistrationAttribute : Attribute { }
