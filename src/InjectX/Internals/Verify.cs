5276e2f9d169a42e8f4e1e7ee87c0f055ee8e269
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-22 18:01:32 -0500

    Refactor Verify.cs

---
+++ src/InjectX/Internals/Verify.cs
@@ -0,0 +1,25 @@
+﻿namespace InjectX.Internals;
+
+[DebuggerStepThrough]
+internal static partial class Verify
+{
+    internal static void NotUnmanagedCall()
+    {
+        if (Assembly.GetEntryAssembly() is null)
+            ThrowInvalidOperation(SR.UnmanagedCall);
+    }
+
+    internal static void NotNull<T>([NotNull] T? argument, [CallerArgumentExpression(nameof(argument))] string paramName = "")
+    {
+        if (argument is null)
+            ThrowArgumentNull(paramName);
+    }
+
+    [DoesNotReturn]
+    private static void ThrowArgumentNull(string? paramName)
+        => throw new ArgumentNullException(paramName);
+
+    [DoesNotReturn]
+    private static void ThrowInvalidOperation(string? message) 
+        => throw new InvalidOperationException(message);
+}
