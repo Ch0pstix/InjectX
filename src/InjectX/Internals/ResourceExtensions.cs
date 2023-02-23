a955c3f248b67c0964b95380fc354833fafcb062
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-23 07:34:34 -0500

    Create ResourceExtensions.cs

---
+++ src/InjectX/Internals/ResourceExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace InjectX.Internals;
+
+internal static class ResourceExtensions
+{
+    internal static string Format(this string str, params object[] args)
+    {
+        return string.Format(str, args);
+    }
+}
