179ce43dd6ae2060562444ec9d27311e2652bcfb
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-23 10:33:23 -0500

    Update TypeNameHelper.cs

--- src/InjectX/Internals/TypeNameHelper.cs
+++ src/InjectX/Internals/TypeNameHelper.cs
@@ -1,22 +1,9 @@
-﻿// Copyright(c) .NET Foundation and Contributors
-//
-// All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-// this file except in compliance with the License. You may obtain a copy of the
-// License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-//
-// From https://github.com/dotnet/aspnetcore/blob/b1dcacabec1aeacef72c9aa2909f1cb49993fa73/src/Shared/TypeNameHelper/TypeNameHelper.cs
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 namespace Microsoft.Extensions.Internal;
 
+[DebuggerStepThrough]
 internal static class TypeNameHelper
 {
     private const char DefaultNestedTypeDelimiter = '+';
@@ -41,7 +28,8 @@ internal static class TypeNameHelper
             { typeof(ushort), "ushort" }
         };
 
-    public static string? GetTypeDisplayName(object item, bool fullName = true)
+    [return: NotNullIfNotNull("item")]
+    public static string? GetTypeDisplayName(object? item, bool fullName = true)
     {
         return item == null ? null : GetTypeDisplayName(item.GetType(), fullName);
     }
