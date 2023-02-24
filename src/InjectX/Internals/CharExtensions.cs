564fc3a6c81bc633d78b16fed5ad2b2990439e02
Isaac Bickel <96277815+Ch0pstix@users.noreply.github.com>
2023-02-24 07:58:02 -0500

    Create CharExtensions.cs

---
+++ src/InjectX/Internals/CharExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace InjectX.Internals;
+
+[DebuggerStepThrough]
+internal static class CharExtensions
+{
+    internal static bool IsAsciiAlphanumeric(this char c)
+    {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
+}
